e5dfbb91745f81e5fe0c76281a04f113278eb5a6
geearevee <grv33431@gmail.com>
2023-05-30 14:57:19 +0530

    search bar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import {useEffect, useState, createContext} from "react";
-import logo from './logo.svg';
-import Navbar from './components/Navbar';
-import './App.css';
-import Aside from './components/Aside'
-import ProductContainer from './components/ProductContainer';
+import { useEffect, useState, createContext } from "react";
+import logo from "./logo.svg";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Aside from "./components/Aside";
+import ProductContainer from "./components/ProductContainer";
 import {
   createBrowserRouter,
   RouterProvider,
-  useAsyncError
+  useAsyncError,
 } from "react-router-dom";
 import ErrorPage from "./components/ErrorPage";
 import Cart from "./components/cart";
@@ -18,71 +18,79 @@ import Payment from "./components/payment";
 export const productContext = createContext();
 const router = createBrowserRouter([
   {
-      path : "/",
-      element : <Root/>,
-      errorElement : <ErrorPage />, 
-      children : [
-          {
-              path : "/",
-              element : <Home />
-          },
-          {
-              path : "/cart",
-              element : <Cart/>,
-          },
-          {
-            path : "/product/:productId",
-            element : <ProductDetails/>,
-          },
-          {
-            path : "/checkout",
-            element : <Payment/>,
-          }
-      ]
-  }
-])
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/product/:productId",
+        element: <ProductDetails />,
+      },
+      {
+        path: "/checkout",
+        element: <Payment />,
+      },
+    ],
+  },
+]);
 function App() {
   const [loader, setLoader] = useState(true);
 
   const [data, setData] = useState([]);
-  const [user,setUser] = useState(null);
+  const [user, setUser] = useState(null);
   const [showNavOptions, setShowNavOptions] = useState(false);
   const [cart, setCart] = useState([]);
   const [searchvalue, setSearchValue] = useState(""); // you are storing search string so inital value can be "" empty string
   const mensUrl = "https://classic-world.onrender.com/MensData";
   const womansUrl = "https://classic-world.onrender.com/WomensData";
-  
+
   const fetchData = async (url) => {
     const query = await fetch(url);
     return query.json();
-  }
+  };
   useEffect(() => {
     const homePageData = async () => {
       let data = await fetchData(mensUrl);
       setData(data);
       setLoader(false);
-    }
+    };
     homePageData();
-  },[])
+  }, []);
   const changeData = async (target) => {
-    if(target.gender === "Female"){
+    if (target.gender === "Female") {
       const data = await fetchData(womansUrl);
       setData(data);
       setLoader(false);
-    }else{
+    } else {
       const data = await fetchData(mensUrl);
 
       console.log(data);
       setData(data);
       setLoader(false);
     }
-  }
-  function searchResult(){
-     let filteredResults = data.filter(result => result.brand === searchvalue);
-     setData(filteredResults);
+  };
+  function searchResult(e) {
+    e.preventDefault();
+    if (searchvalue === "") {
+      console.log("empty");
+      changeData("male");
+      return null;
+    }
+    let filteredResults = data.filter((result) =>
+      result.brand.toLowerCase().includes(searchvalue.toLowerCase())
+    );
+    setData(filteredResults);
   }
   // setLoader(false);
-  const[cartvalue, setCartvalue] = useState(0);
+  const [cartvalue, setCartvalue] = useState(0);
   const value = {
     data,
     setData,
@@ -99,8 +107,8 @@ function App() {
     setLoader,
     searchvalue,
     setSearchValue,
-    searchResult
-  }
+    searchResult,
+  };
   return (
     <productContext.Provider value={value}>
       <RouterProvider router={router} />
@@ -109,5 +117,3 @@ function App() {
 }
 
 export default App;
-
-
